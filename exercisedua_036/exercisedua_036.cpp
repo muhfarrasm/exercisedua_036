d37559a2e7698a8d1d622416576342e85607b80c
muhfarrasm <muhammadfarrasm@gmail.com>
2023-06-07 14:47:26 +0700

    Paling Fix

--- exercisedua_036/exercisedua_036.cpp
+++ exercisedua_036/exercisedua_036.cpp
@@ -1,26 +1,79 @@
 #include <iostream>
-#include "Kumpulan Prosedur.h"
 using namespace std;
 
-int main()//program utama
-{
-	bidangDatar* BD;
+class bidangdatar {
+private:
+    int x; // variabel untuk menyimpan input dari lingkaran maupun bujursangkar
+public:
+    bidangdatar() { // constructor
+        x = 0;
+    }
+    virtual void input() = 0; // fungsi yang menerima input dan mengirim input melalui fungsi setX untuk disimpan di x
+    virtual float Luas(int a) = 0; // fungsi untuk menghitung luas
+    virtual float Keliling(int a) = 0; // fungsi untuk menghitung keliling
+    void setX(int a) { // fungsi untuk memberi/mengirim nilai pada x
+        this->x = a;
+    }
+    int getX() { // fungsi untuk membaca/mengambil nilai dalam x
+        return x;
+    }
+};
 
-	//Bagian Dari Lingkaran
-	cout << "Lingkaran Dibuat ";
-	BD = new lingkaran();
-	BD->input();
-	int r = BD->getX();
-	cout << "Luas Lingkaran = " << BD->Luas(r) << endl;
-	cout << "Keliling Lingkaran = " << BD->Keliling(r) << endl;
+class lingkaran : public bidangdatar {
+public:
+    void input() {
+        int r;
+        cout << "Masukkan jari-jari: ";
+        cin >> r;
+        setX(r);
+    }
 
-	//Bagian Dari Bujursangkar
-	cout << "Bujursangkar Dibuat";
-	BD = new lingkaran();
-	BD->input();
-	int s = BD->getX();
-	cout << "Luas Lingkaran = " << BD->Luas(s) << endl;
-	cout << "Keliling Lingkaran = " << BD->Keliling(s) << endl;
+    float Luas(int r) {
+        return 3.14 * r * r;
+    }
 
-	return 0;
+    float Keliling(int r) {
+        return 2 * 3.14 * r;
+    }
+};
+
+class bujursangkar : public bidangdatar {
+public:
+    void input() {
+        int s;
+        cout << "Masukkan sisi: ";
+        cin >> s;
+        setX(s);
+    }
+
+    float Luas(int s) {
+        return s * s;
+    }
+
+    float Keliling(int s) {
+        return 4 * s;
+    }
+};
+
+int main() {
+    bidangdatar* bd;
+
+    //Bagian Lingkaran
+    cout << "Lingkaran dibuat" << endl;
+    bd = new lingkaran(); //menunjukan jika lingkaran merupakan turunan dari class Bidang datar
+    bd->input();//menunjukan jika lingkaran merupakan turunan dari class Bidang datar
+    int r = bd->getX();
+    cout << "Luas Lingkaran = " << bd->Luas(r) << endl;
+    cout << "Keliling Lingkaran = " << bd->Keliling(r) << endl;
+
+    //Bagian Bujursangkar
+    cout << "\nBujursangkar dibuat" << endl;
+    bd = new bujursangkar();
+    bd->input();
+    int s = bd->getX();
+    cout << "Luas Bujursangkar = " << bd->Luas(s) << endl;
+    cout << "Keliling Bujursangkar = " << bd->Keliling(s) << endl;
+
+
+    return 0;
 }
