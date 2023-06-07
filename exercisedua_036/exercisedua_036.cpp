6a5f0714df53f46d82b09dc4768a9d429d676d44
muhfarrasm <muhammadfarrasm@gmail.com>
2023-06-07 13:40:29 +0700

    membuat class induk bidang datar

--- exercisedua_036/exercisedua_036.cpp
+++ exercisedua_036/exercisedua_036.cpp
@@ -1,20 +1,19 @@
-// exercisedua_036.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;class bidangDatar {
+private:
+	int x; //variabel untuk menyimpan input dari lingkaran maupun bujursangkar
+public:
+	bidangDatar() { //constructor
+		x = 0;
+	}
+	virtual void input() {} // fungsi yang menerima input dan mengirim input melalui fungsi setX untuk disimpan di x
+		virtual float Luas(int a) { return 0; } //fungsi untuk menghitung luas
+	virtual float Keliling(int a) { return 0; } //fungsi untuk menghitung keliling
+	void setX(int a) { //fungsi untuk memberi/mengirim nilai pada x
+		this->x = a;
+	}
+	int getX() { //fungsi untuk membaca/mengambil nilai dalam x
+		return x;
+	}
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
